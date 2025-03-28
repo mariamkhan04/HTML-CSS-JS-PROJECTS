35ff2c43f10451de2838c11b9c3878b73b37ca0a
mariamkhan04 <125134327+mariamkhan04@users.noreply.github.com>
2025-03-28 18:24:07 +0500

    `Update fetchRandomQuotes function to use zenquotes API instead of quotable.io` and updated greeting function to generate random greetings

--- Random Quote Generator & Greeting App/scripts.js
+++ Random Quote Generator & Greeting App/scripts.js
@@ -3,18 +3,37 @@ const greeting = document.querySelector('h2');
 const quotebtn = document.getElementById('quote-btn');
 const quotePara = document.getElementById('quote');
 
+// Array of multiple greeting messages
+const greetings = [
+    "Hello, World!",
+    "Good day to you!",
+    "Hey there, buddy!",
+    "Stay positive, stay happy!",
+    "Wishing you a great day!",
+    "Keep smiling!",
+    "You got this!"
+];
+
 const handleGreeting = () =>{
-    const message = prompt("Enter a new greeting msg:");
-    if(message){
-        greeting.textContent = message;
-    }
+    // const message = prompt("Enter a new greeting msg:");
+    // if(message){
+    //     greeting.textContent = message;
+    // }
+    const randomIndex = Math.floor(Math.random() * greetings.length);
+    greeting.textContent = greetings[randomIndex];
 }
 
 const fetchRandomQuotes = async () =>{
     try{
-        const response = await fetch('http://api.quotable.io/random');
+        const response = await fetch('https://corsproxy.io/?url=https://zenquotes.io/api/random');
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error! Status: ${response.status}`);
+        }
+
         const quote = await response.json();
-        quotePara.textContent = quote.content;
+        // console.log(quote);
+        quotePara.textContent =  `${quote[0].q} - ${quote[0].a}`;
     }catch(error){
         quotePara.textContent = "Failed to load Quote. Try again!";
         console.error("Error fetching quote:", error);
